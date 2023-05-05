c79063ada8cfe764e3993df22eab822bca1a8892
carbonemys <baudoin_@hotmail.com>
2023-05-05 15:06:45 +0200

    add channel yt detail option

--- tools/youtube_detail.py
+++ tools/youtube_detail.py
@@ -12,5 +12,13 @@ def youtube_detail(youtube_url, youtube_detail):
         video_title = title_element.text.strip()
         video_title = video_title.replace(' - YouTube', '')
         return video_title
+    elif (youtube_detail == 'channel'):
+        channel_element = soup.find('span', {'itemprop': 'author'})
+        if (channel_element):
+            channel_name = channel_element.find('link', {'itemprop': 'name'})['content']
+            channel_url = 'https://www.youtube.com' + channel_element.find('link', {'itemprop': 'url'})['href']
+            return f'<a href="{channel_url}">{channel_name}</a>'
+        else:
+            return 'Error: channel not found'
     else:
         return 'Error: no detail specified'
